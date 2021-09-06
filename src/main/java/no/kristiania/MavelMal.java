0f3e7e4496151db6572621a4c292614e7ca445ed
Svebbeee <sandlandsverre@gmail.com>
2021-09-06 18:08:54 +0200

    Initial commit

---
+++ src/main/java/no/kristiania/MavelMal.java
@@ -0,0 +1,7 @@
+package no.kristiania;
+
+public class MavelMal {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
---
+++ src/test/java/no/kristiania/MavelMalTest.java
@@ -0,0 +1,12 @@
+package no.kristiania;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MavelMalTest {
+
+    @Test
+    void DummyTest(){
+        assertEquals(42, 6*7);
+    }
+}
